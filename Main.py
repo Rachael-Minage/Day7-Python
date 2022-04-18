4568624408fe7d45812edd3b36b1b7445c3cfd37
Rachael-Minage <lauryneminagge@gmail.com>
2022-04-18 22:39:46 +0300

    first commit

---
+++ Main.py
@@ -0,0 +1,42 @@
+from art import logo
+print(logo)
+from words import word_list
+import random
+chosen_word= random.choice(word_list)
+print(chosen_word)
+# lives= len(stages)-1
+lives=6
+
+display=[]
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display+="_"
+    print(display)
+
+end_of_game= False
+from art import stages
+while end_of_game==False:
+    guess = input("Guess a letter\n").lower()
+    if guess in display:
+        print(f"You already guessed {guess}")
+    for position in range(word_length):
+        letter=chosen_word[position]
+        if letter==guess:
+            display[position]= letter
+    print(display)
+    if guess not in chosen_word:
+        print(f"{guess} not in letter. You lose a life")
+        lives-=1
+        if lives ==0:
+            end_of_game=True
+            print("You lose")
+    if "_" not in display:
+        end_of_game =True
+        print("You win")
+    print(stages[lives])    
+
+
+    
+
+
+
